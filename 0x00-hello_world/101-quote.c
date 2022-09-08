50e2d766324a0a60833cc234d0fb917f4b7a856b
elielreich23 <elielreich23@yahoo.com>
2022-09-08 16:48:45 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: Always 1
 */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"59);
+	write("and that piece of art is useful\ - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
+
