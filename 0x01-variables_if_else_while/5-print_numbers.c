1dca4ab6349404a4d315bad16eeb1b6516c9f6d0
elielreich23 <elielreich23@yahoo.com>
2022-09-09 11:11:01 -0700

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everything works fine
+ *
+*/
+
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; ++l)
+	{
+		printf("%d", l);
+	}
+	putchar(10);
+	return (0);
+}
