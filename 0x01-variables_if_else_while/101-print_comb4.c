a18781978364a48430d641a968a72814b88487de
elielreich23 <elielreich23@yahoo.com>
2022-09-09 12:09:29 -0700

    m

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,4 +10,23 @@ int main(void)
 {
 	int l, l1, l2;
 
-	for 
+	for (l = 48; l <= 56; l++)
+	{
+		for (l1 = l + 1; l1 <= 57; l1++)
+		{
+			for (l2 = l1 + 1; l2 <= 57; l2++)
+			{
+				putchar(l);
+				putchar(l1);
+				putchar(l2);
+				if (l != 55 || l1 != 56 || l2 != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
