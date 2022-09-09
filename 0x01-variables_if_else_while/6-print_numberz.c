81db732d2a9cc251927e409cf1ecd169d8d8f97f
elielreich23 <elielreich23@yahoo.com>
2022-09-09 11:14:57 -0700

    program to print number using ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everything works fine
+ *
+*/
+
+int main(void)
+{
+	int l;
+
+	for (l = 48; l < 58; ++l)
+	{
+		putchar(l);
+	}
+	putchar(10);
+	return (0);
+}
