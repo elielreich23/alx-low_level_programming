6c5e602d6aa55d1c1f8c790b0d99cab4b193441a
elielreich23 <elielreich23@yahoo.com>
2022-09-09 11:59:20 -0700

    k

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everything works fine
+ *
+*/
+
+int main(void)
+{
+	int l, l1;
+
+	for (l = 48; l <= 56; l++)
+	{
+		for (l1 = l + 1; l1 <= 57; l1++)
+		{
+			putchar(l);
+			putchar(l1);
+			if (l != 56 && l1 != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int l, l1, l2;
+
+	for 
