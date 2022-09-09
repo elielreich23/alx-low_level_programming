88df64ddd6032a177544f96256a830b0add93700
elielreich23 <elielreich23@yahoo.com>
2022-09-09 10:53:33 -0700

    k

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everything works fine
+ *
+*/
+
+int main(void)
+{
+	int l;
+
+	for (l = 97; l <= 122; ++l)
+	{
+		if (l != 101 l != 113)
+		{
+			putchar(l);
+		}
+	}
+	putchar(10);
+	return (0);
+}
