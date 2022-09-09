8bde1175ddb463b50981e54b0e452ac65db1575d
elielreich23 <elielreich23@yahoo.com>
2022-09-09 10:34:54 -0700

    l

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Program to print alphabet letters in all cases followed by new line
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 97; l <=122; ++l)
+		putchar(l);
+	for (l = 60; l <= 90; ++l)
+		putchar(l);
+	putchar(10);
+	return (0);
+}	
