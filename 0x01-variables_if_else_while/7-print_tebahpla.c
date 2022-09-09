730aa779824985d9acdb27dcac56844959fb949c
elielreich23 <elielreich23@yahoo.com>
2022-09-09 11:18:38 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: returns 0 when everything works fine
+ * 
+*/
+
+int main (void)
+{
+	int l;
+
+	for (l = 122; l >=97; --l)
+	{
+		putchar(l);
+	}
+	putchar(10);
+	return (0);
+}
