2f5e8f34ef5a5bb57e60fc5a382210efea6552e9
elielreich23 <elielreich23@yahoo.com>
2022-09-12 09:01:09 -0700

    update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all combinations of two two digits with,
+ * and space followed by new line
+ *
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 <= 98; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig <= 99; dig2++)
+		{
+			putchar((dig1 / 10) + '0');
+			putchar((dig 1 % 10) + '0');
+			putchar(32);
+			putchar((dig2 / 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 / 10 != 9 || dig1 % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
