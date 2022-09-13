9047cfab8ee5d3a8f5e5ee32ee28e4ba6df10971
elielreich23 <elielreich23@yahoo.com>
2022-09-13 05:47:18 -0700

    nested_loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function that print the alphabet 10 times
+ *
+ * Return: return 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int l, counter = 0;
+
+	while (counter <= 9)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+			_putchar(10);
+		}
+	}
+}
