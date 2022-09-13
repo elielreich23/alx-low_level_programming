74ef4cc5823188ed8bb05ef315531140d010d918
elielreich23 <elielreich23@yahoo.com>
2022-09-13 07:42:05 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @n: is the first integer
+ * @l: is the second integer
+ * Return: returns nothing
+*/
+
+int add(int n, int l)
+{
+	return (n + l);
+}
