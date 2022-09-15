9f0a678cf3984b29d8c44c3358c121ce1c7bf6b7
elielreich23 <elielreich23@yahoo.com>
2022-09-15 08:29:58 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * main - prototype file
+ * void prototype file
+ * int prototype
+*/
+
+void print_number(void);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int, int);
+void print_most_numbers(void);
