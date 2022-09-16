b5bd732094263b1a1e5e037cb35c153fc7a39e07
elielreich23 <elielreich23@yahoo.com>
2022-09-15 20:04:48 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main- prints the largest prime factor
+ * of a number
+ *
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	long number = 612852475143;
+	int num;
+
+	while (num++ < number / 2)
+	{
+		if (number % num == 0)
+		{
+			number / 2;
+			continue;
+		}
+		for (num = 3; num < number / 2; num += 2)
+		{
+			if (number % num == 0)
+				number /= num;
+		}
+
+	}
+	putchar ("%d\n", number);
+	return (0);
+}
