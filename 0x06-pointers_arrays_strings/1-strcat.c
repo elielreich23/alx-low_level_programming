a10aa2e59ac7e06d2e3e06812a34b56e7335d00d
elielreich23 <elielreich23@yahoo.com>
2022-09-21 16:07:56 -0700

    arrays

--- 0x06-pointers_arrays_strings/1-strcat.c
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+include "main.h"
 
 /**
  * _strcat - concatenates  the string pointed to by @src to
@@ -8,3 +8,16 @@ nclude "main.h"
  *
  * Return: returns poiner to @dest
 */
+
+char *_strcat(char *dest, char *src)
+{
+	int ind = 0, dest_len = 0;
+
+	while (dest[ind++])
+		dest_len++;
+
+	for (index = 0; src[ind]; ind++)
+		dest[dest_len++] = src[ind];
+
+	return (dest);
+}
