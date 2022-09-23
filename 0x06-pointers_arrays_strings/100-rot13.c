0c86efa30520fd6689b8c0f47a685c83a4d659ef
elielreich23 <elielreich23@yahoo.com>
2022-09-23 09:58:37 -0700

    main-updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,48 +2,28 @@
 
 /**
  * rot13 - rot13 encoder
- * @str: string to be encoded
+ * @s: string to be encoded
  *
  * Return: address of the encoded string
 */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int i = 0;
+	int count = 0, i;
+	char alphabeth[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (str[i] != '\0')
+	while (*(s + count) != '\0')
 	{
-		str[i] = transform_2(str[i]);
-		i++;
-	}
-	return (str);
-}
-
-/**
- * transform_2 - helper function to map a letter with it's rot13 encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
-*/
-
-char transform_2(char x)
-{
-	char one[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char two[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i = 0;
-	char replacement = x;
-
-	while (i < 52)
-	{
-		if (x == one[i])
+		for (i = 0; i < 52; i++)
 		{
-			replacement = two[i];
-			break;
+			if (*(s + count) == alphabeth[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
 		}
-		i++;
-
+		count++;
 	}
-	return (replacement);
+	return (s);
 }
-
-
