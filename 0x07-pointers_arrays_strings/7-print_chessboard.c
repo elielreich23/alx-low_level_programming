aae963f1bd6ecfaa61e5656be07c1e428c2c18df
elielreich23 <elielreich23@yahoo.com>
2022-09-26 10:59:30 -0700

    function

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
 /**
  * print_chessboard - a function that prints the chessboard.
  * @a: array containing board elements
- * returns nothing
+ * Return: void
 */
 
 void print_chessboard(char (*a)[8])
