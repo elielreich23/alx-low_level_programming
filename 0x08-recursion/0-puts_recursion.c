5abecc9fe68bddb67cf2301e655c894c0690cf2d
elielreich23 <elielreich23@yahoo.com>
2022-09-28 12:45:05 -0700

    recursion 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar ('\n');
+	if (*s != '\0')
+		_putchar(*s);
+		_put_recursion(s + 1);
 	else
 	{
-		_putchar(s[0]);
-		_put_recursion(s + 1);
+		_putchar('\n');
 	}
 }
