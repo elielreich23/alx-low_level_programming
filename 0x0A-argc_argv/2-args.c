3b1f7f19c3a0020ee48c6096f7dfb4277d499f83
elielreich23 <elielreich23@yahoo.com>
2022-09-30 12:52:03 -0700

    exrcise 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it received
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int b;
+
+	for (b = 0;  b < argc; b++)
+		printf ("%s\n", argv[i]);
+	return (0);
+}
