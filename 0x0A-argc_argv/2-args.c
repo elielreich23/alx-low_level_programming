3c4c715a8d56fa10c2a98b4cdc77c6e53cb2848c
elielreich23 <elielreich23@yahoo.com>
2022-09-30 12:54:19 -0700

    exrcise 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,6 +12,6 @@ int main(int argc, char *argv[])
 	int b;
 
 	for (b = 0;  b < argc; b++)
-		printf ("%s\n", argv[i]);
+		printf ("%s\n", argv[b]);
 	return (0);
 }
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints multiplication
+ * of two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: returns 0
+*/
