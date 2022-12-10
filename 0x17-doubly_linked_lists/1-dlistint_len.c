f3735fbdfe1b41586157950ba7b21409fa05bcbe
elielreich23 <elielreich@yahoo.com>
2022-12-09 22:17:24 -0300

    doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,13 +6,13 @@
  *
  * Return: The number of elements in the list.
  */
-
-size_t dlistint_len(const dlistint_t *h){
-    size_t nodes = 0
-    while (h)
-    {
-        nodes++;
-        h = h->next;
-    }
-    return (nodes)
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t nodes = 0
+while (h)
+{
+nodes++;
+h = h->next;
+}
+return (nodes)
 }
